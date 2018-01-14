f85b3b292c40d9c67f0c788e0f595a1024cc7ce9
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-14 14:56:16 -0200

    Assoc test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,8 +1,17 @@
-import { sum } from '../src'
+import { sum, assoc } from '../src'
 
 describe('curried sum', () => {
   it('can sum 2 positive numbers', () => {
     expect(sum(1, 2)).toBe(3)
     expect(sum(1)(2)).toBe(3)
+  })
+})
+
+describe('curried assoc', () => {
+  it('can assoc a key-val into an object', () => {
+    expect(assoc('name', 'Rafael', { age: 12 })).toEqual({
+      name: 'Rafael',
+      age: 12,
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as R from 'ramda'
 
 export const sum = R.curry((a: number, b: number): number => a + b)
 
-export const assoc = (key: any) => (val: any) => (map: object) => ({
+export const assoc = R.curry((key: any, val: any, map: object): object => ({
   ...map,
   [key]: val,
-})
+}))
